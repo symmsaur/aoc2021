08e8dadde9c72d8b51d654c1bd2c09c4c50e1085
Jonathan Karlsson <karlsjona@gmail.com>
2021-12-01 06:12:40 +0100

    Scaffolding

---
+++ next.fish
@@ -0,0 +1,20 @@
+#!/bin/fish
+
+# Figure out the next days number
+set dirs (ls -F | grep '^d.*$')
+set nums
+for d in $dirs
+    set nums $nums (echo $d | cut --characters=2-3)
+end
+set num (math $nums[-1] + 1)
+set padded_num (printf "%02d" $num)
+
+# Create new dir and prep input for the next day
+set new_dir d$padded_num
+mkdir -p $new_dir
+curl --cookie session=(cat session) \
+  https://adventofcode.com/2020/day/$num/input \
+  > $new_dir/input
+
+cd d$padded_num
+cargo init
