aa02cb236226698bddc1aa239645ba93dbb171bc
Jonathan Karlsson <karlsjona@gmail.com>
2021-12-04 23:33:01 +0100

    Day 3

---
+++ d03/main.clj
@@ -0,0 +1,63 @@
+(ns noob
+  (:gen-class)
+  (:require [clojure.string :as string]
+            [clojure.edn :as edn]))
+
+(defn read-input []
+  (->> (slurp "test")
+       string/split-lines))
+
+(defn transpose [vv]
+  (apply mapv vector vv))
+
+(defn gamma []
+  (->> (read-input)
+       (transpose)
+       (map frequencies)
+       (map (fn [kv] (key (apply max-key val kv))))
+       (string/join)
+       (#(read-string (str "2r" %)))))
+
+(defn epsilon []
+  (->> (read-input)
+       (transpose)
+       (map frequencies)
+       (map (fn [kv] (key (apply min-key val kv))))
+       (string/join)
+       (#(read-string (str "2r" %)))))
+
+(println "part1"
+         (* (gamma) (epsilon)))
+
+(defn max-at-index [l i]
+  (->> (map #(nth % i) l)
+       (frequencies)
+       (#(key (apply max-key val %)))))
+
+(defn min-at-index [l i]
+  (->> (map #(nth % i) l)
+       (frequencies)
+       (#(key (apply min-key val %)))))
+
+(defn scan-filter [sel-fn l]
+  (loop [i 0
+         l l]
+    (if (< i (count (first l)))
+      (recur (+ 1 i)
+             (filter #(= (nth % i)
+                         (sel-fn l i)) l))
+      (first l))))
+
+(defn rating [sel-fn]
+  (->> (read-input)
+       (scan-filter sel-fn)
+       (#(read-string (str "2r" %)))))
+
+(defn oxygen []
+  (rating max-at-index))
+
+(defn co-2 []
+  (rating min-at-index))
+
+(println "part2"
+         (* (oxygen) (co-2)))
